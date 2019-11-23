4b63af548d4dc904d149c15010a1155e214061cc
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2019-11-23 01:17:26 -0600

    Create PopulatingNextRightPointersInEachNode-II.java

---
+++ PopulatingNextRightPointersInEachNode-II.java
@@ -0,0 +1,87 @@
+/*
+
+Populating Next Right Pointers in Each Node II
+Given a binary tree
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+
+Example:
+
+Input: {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":null,"next":null,"right":{"$id":"6","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+
+Output: {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":null,"right":null,"val":7},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"6","left":null,"next":null,"right":{"$ref":"5"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"6"},"val":1}
+
+Explanation: Given the above binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. 
+
+Note:
+-You may only use constant extra space.
+-Recursive approach is fine, implicit stack space does not count as extra space for this problem.
+
+Reference: https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
+*/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val,Node _left,Node _right,Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+    int currentLevel = -1;
+    public Node connect(Node root) {
+
+        if(root == null)
+            return root;
+        
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        Queue<Integer> level = new LinkedList<>();
+        level.add(0);
+        
+        Node previous = null;
+        
+        while(!queue.isEmpty()) {
+            Node node = queue.poll();
+            int nodeLevel = level.poll();
+            
+            if(nodeLevel > currentLevel) {
+                currentLevel = nodeLevel;
+                previous = null;
+            }
+            
+            node.next = previous;
+            previous = node;
+
+            if(node.right != null) {
+                queue.add(node.right);
+                level.add(currentLevel+1);
+            }
+            if(node.left != null) {
+                queue.add(node.left);
+                level.add(currentLevel+1);
+            }
+        }
+        return root;
+    }
+}
