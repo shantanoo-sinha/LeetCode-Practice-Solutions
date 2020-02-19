222a9574da27ab5e026813817bfa91be1e58d9db
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2020-02-19 10:02:31 -0600

    Update best-time-to-buy-and-sell-stock-ii.java

--- best-time-to-buy-and-sell-stock-ii.java
+++ best-time-to-buy-and-sell-stock-ii.java
@@ -1,6 +1,12 @@
 /*
 
-Reference: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/ engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+Reference: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
 
 Example 1:
 
