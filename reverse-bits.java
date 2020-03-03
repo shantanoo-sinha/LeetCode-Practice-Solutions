d7c9797ee75f5160c3e0befb59eba5bae51581d9
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2020-03-03 10:00:36 -0600

    Update reverse-bits.java

--- reverse-bits.java
+++ reverse-bits.java
@@ -21,3 +21,17 @@ Note:
     In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above the input represents the signed integer -3 and the output represents the signed integer -1073741825.
 
 */
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+int result=0;
+        for(int i=0; i<32; i++){
+            result = result << 1;
+result = result + (n&1);
+            n = n>>>1;
+
+}
+       return result; 
+    }
+}
