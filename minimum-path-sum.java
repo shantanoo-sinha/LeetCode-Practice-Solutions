257675980def1a057eafaea3a456f9d28012aba0
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2020-04-19 23:44:21 -0500

    Update minimum-path-sum.java

--- minimum-path-sum.java
+++ minimum-path-sum.java
@@ -28,9 +28,9 @@ Explanation: Because the path 1→3→1→1→1 minimizes the sum.
 		
     public static int min(int[][]grid, int row, int col){
         if(row == 0 && col == 0) return grid[row][col]; // this is the exit of the recursion
-        if(row == 0) return grid[row][col] + min(grid, row, col - 1); /** when we reach the first row, we could only move horizontally.*/
-        if(col == 0) return grid[row][col] + min(grid, row - 1, col); /** when we reach the first column, we could only move vertically.*/
-        return grid[row][col] + Math.min(min(grid, row - 1, col), min(grid, row, col - 1)); /** we want the min sum path so we pick the cell with the less value */
+        if(row == 0) return grid[row][col] + min(grid, row, col - 1); // when we reach the first row, we could only move horizontally.
+        if(col == 0) return grid[row][col] + min(grid, row - 1, col); // when we reach the first column, we could only move vertically.
+        return grid[row][col] + Math.min(min(grid, row - 1, col), min(grid, row, col - 1)); // we want the min sum path so we pick the cell with the less value
 	}
 }*/
 
