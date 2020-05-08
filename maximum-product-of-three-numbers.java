8a267d5f5b66a2afd4ecf84eadcfdca658ddc76a
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2020-05-08 13:23:25 -0500

    Update maximum-product-of-three-numbers.java

--- maximum-product-of-three-numbers.java
+++ maximum-product-of-three-numbers.java
@@ -20,6 +20,7 @@ Note:
     Multiplication of any three numbers in the input won't exceed the range of 32-bit signed integer.
 
 */
+//O(n*log n)
 class Solution {
     public int maximumProduct(int[] nums) {
         if(nums == null || nums.length < 3)
@@ -27,5 +28,36 @@ class Solution {
 
         Arrays.sort(nums);
         return Math.max(nums[nums.length-1] * nums[nums.length-2] * nums[nums.length-3], nums[0]*nums[1]*nums[nums.length-1]);
+    }
+}
+
+//O(n)
+class Solution {
+    public int maximumProduct(int[] nums) {
+        if(nums == null || nums.length < 3)
+            return 0;
+
+        int max1=Integer.MIN_VALUE, max2=Integer.MIN_VALUE, max3=Integer.MIN_VALUE;
+        int min1=Integer.MAX_VALUE, min2=Integer.MAX_VALUE;
+        
+        for(int i=0; i<nums.length; i++) {
+            if (nums[i] <= min1) {
+                min2 = min1;
+                min1 = nums[i];
+            } else if (nums[i] <= min2) {     // n lies between min1 and min2
+                min2 = nums[i];
+            }
+            if (nums[i] >= max1) {            // n is greater than max1, max2 and max3
+                max3 = max2;
+                max2 = max1;
+                max1 = nums[i];
+            } else if (nums[i] >= max2) {     // n lies betweeen max1 and max2
+                max3 = max2;
+                max2 = nums[i];
+            } else if (nums[i] >= max3) {     // n lies betwen max2 and max3
+                max3 = nums[i];
+            }
+        }
+        return Math.max(min1 * min2 * max1, max1 * max2 * max3);
     }
 }
