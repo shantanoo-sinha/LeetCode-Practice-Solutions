5b2e733da620e5aa7171bfd802814802c4285a54
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2020-05-19 00:06:35 -0500

    Update maximum-sum-circular-subarray.java

--- maximum-sum-circular-subarray.java
+++ maximum-sum-circular-subarray.java
@@ -42,3 +42,16 @@ Note:
     1 <= A.length <= 30000
 
 */
+class Solution {
+    public int maxSubarraySumCircular(int[] A) {
+        int total = 0, maxSum = Integer.MIN_VALUE, curMax = 0, minSum = Integer.MAX_VALUE, curMin = 0;
+        for (int a : A) {
+            curMax = Math.max(curMax + a, a);
+            maxSum = Math.max(maxSum, curMax);
+            curMin = Math.min(curMin + a, a);
+            minSum = Math.min(minSum, curMin);
+            total += a;
+        }
+        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
+    }
+}
