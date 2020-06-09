7a8dcc50bae6782cbc964da91af09facb7ae133c
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2020-06-09 17:29:12 -0500

    Update is-subsequence.java

--- is-subsequence.java
+++ is-subsequence.java
@@ -32,3 +32,16 @@ Constraints:
 Both strings consists only of lowercase characters.
 
 */
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i=0;
+        for(char c : t.toCharArray()) {
+            if(i>=s.length())
+                break;
+            else if(s.charAt(i) == c)
+                i++;
+        }
+        return i==s.length() ? true : false;
+    }
+}
