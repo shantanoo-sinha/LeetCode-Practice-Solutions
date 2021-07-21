637a5c7b3f827373d7b6d01cb075e6ca6b320555
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2021-07-21 00:12:37 -0500

    Update insert-into-a-binary-search-tree.java

--- insert-into-a-binary-search-tree.java
+++ insert-into-a-binary-search-tree.java
@@ -71,3 +71,18 @@ class Solution {
         return newRoot;
     }
 }
+
+//Recursive Solution
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null)
+            return new TreeNode(val);
+        
+        if(val < root.val)
+            root.left = insertIntoBST(root.left, val);
+        else if(val > root.val)
+            root.right = insertIntoBST(root.right, val);
+        
+        return root;
+    }
+}
